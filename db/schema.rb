bfb0575587f68be804f43ae2f0f6fc7f8a444784
Dysp <dysperen@gmail.com>
2017-03-14 17:10:53 +0100

    Updated DB to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170313165907) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bloodsamples", force: :cascade do |t|
     t.integer  "paraclinical_id"
     t.boolean  "creatinine"
