8e24e2dfc9e6d4379b1a8472a013dec43bd1214e
Dysp <dysperen@gmail.com>
2017-03-21 20:46:43 +0100

    Alright

--- db/migrate/20170316153838_create_differentials.rb
+++ db/migrate/20170316153838_create_differentials.rb
@@ -1,4 +1,4 @@
-class CreateCategorizations < ActiveRecord::Migration[5.0]
+class CreateDifferentials < ActiveRecord::Migration[5.0]
   def change
     create_table :differentials do |t|
       t.integer :illness_id
--- db/migrate/20170316155720_renaming_differentials.rb
+++ db/migrate/20170316155720_renaming_differentials.rb
@@ -1,4 +1,4 @@
-class RenamingCategories < ActiveRecord::Migration[5.0]
+class RenamingDifferentials < ActiveRecord::Migration[5.0]
   def change
     rename_column :differentials, :diff_id, :differential_id
   end
