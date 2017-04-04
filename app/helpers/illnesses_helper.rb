1957b35e215b2dae69d2676b7d5ccb79a9d71b36
Dysp <dysperen@gmail.com>
2017-04-04 21:21:30 +0200

    Refactoring and changes to view

--- app/helpers/illnesses_helper.rb
+++ app/helpers/illnesses_helper.rb
@@ -7,12 +7,9 @@ module IllnessesHelper
     end
   end
 
-  def imaging_indicated
-    #TODO: Virker ikke. Returnerer altid true eller false.
-    if !@illness.test.nil?
-      @illness.test.attributes.all? do |key, value|
-        key.inspect
-      end
-    end
+  def imaging_indicated(model)
+    ignored_attrs = {'id' => 1, 'created_at' => 1, 'updated_at' => 1}
+    model.attributes.all?{|k,v| v.blank? || ignored_attrs[k]}
   end
+
 end
